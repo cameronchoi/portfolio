6002eee982d5d2dfa76269962aaad0951342f928
Cameron Choi <cameron.choi99@gmail.com>
2021-10-09 16:15:27 +1100

    Add links to socials in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,59 @@
 import React from "react";
 import { StyledFooter, StyledIcon } from "./styles/Footer.styled";
-import { faGithub, faLinkedinIn, faMediumM } from "@fortawesome/free-brands-svg-icons";
-import {faEnvelope} from "@fortawesome/free-regular-svg-icons"
+import {
+  faGithub,
+  faLinkedinIn,
+  faMediumM,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 export const Footer = () => {
   return (
     <StyledFooter>
-      <h3>Fully designed and developed by Cameron Choi</h3>
+      <h3>
+        Fully{" "}
+        <a
+          href="https://github.com/cameronchoi/portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          designed
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://github.com/cameronchoi/portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          developed
+        </a>{" "}
+        by Cameron Choi
+      </h3>
       <div>
-        <StyledIcon icon={faGithub} size="3x" />
-        <StyledIcon icon={faLinkedinIn} size="3x"/>
-        <StyledIcon icon={faMediumM} size="3x"/>
-        <StyledIcon icon={faEnvelope} size="3x"/>
+        <a
+          href="https://github.com/cameronchoi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <StyledIcon icon={faGithub} size="3x" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/cameronchoi/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <StyledIcon icon={faLinkedinIn} size="3x" />
+        </a>
+        <a
+          href="https://medium.com/@cameronjchoi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <StyledIcon icon={faMediumM} size="3x" />
+        </a>
+        <a href="mailto:cameronjchoi@gmail.com">
+          <StyledIcon icon={faEnvelope} size="3x" />
+        </a>
       </div>
     </StyledFooter>
   );
